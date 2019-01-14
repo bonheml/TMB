896df96e380dc8f5e4afee1561cc3bf35ef0d12f
Lisa Bonheme <lisa.bonheme@epitech.eu>
2019-01-14 10:18:31 +0100

    Add basic unit test for CI purposes

---
+++ web_api/tests/api_test.py
@@ -0,0 +1,8 @@
+from app import app
+
+test_app = app.test_client()
+
+def test_home():
+    result = test_app.get('/')
+    assert result.status_code == 200
+    assert result.data == b'Main page of TMB api'

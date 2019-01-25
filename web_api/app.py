567712f8e48ed053e1bce6c6be0b8819df4369c5
Lisa Bonheme <lisa.bonheme@epitech.eu>
2019-01-25 19:08:14 +0100

    Implement new app version
    - validate input requests and auto format output requests
    - update related unit tests

--- web_api/app.py
+++ web_api/app.py
@@ -1,11 +1,59 @@
 #!flask/bin/python
+import json
+
+from bird_detection import BirdDetector
+from cerberus import Validator
 from flask import Flask
+from flask_restplus import Api, fields, Resource
+from werkzeug.datastructures import FileStorage
 
 app = Flask(__name__)
+api = Api(app, version='1.0', title="That's my bird", validate=True,
+          description="An API providing bird recognition from song and/or "
+                      "captions using machine learning.")
 
-@app.route('/')
-def index():
-    return "Hello world"
+
+result_fields = api.model('result', {
+    'species': fields.String,
+    'score': fields.Float
+})
+
+output = api.model('response', {
+    'result': fields.List(fields.Nested(result_fields), required=True)
+})
+
+
+def result_validator(value):
+    result_schema = {
+        'species': {'required': True, 'type': 'string'},
+        'score': {'required': True, 'type': 'float', 'min': 0.0, 'max': 1.0}
+    }
+    values = json.loads(value)
+    validator = Validator(result_schema)
+    for v in values:
+        if not validator.validate(v):
+            raise ValueError(json.dumps(validator.errors))
+    return values
+
+
+parser = api.parser()
+parser.add_argument('file', location='files', type=FileStorage, required=True)
+parser.add_argument('result', location='form', type=result_validator,
+                    required=False)
+
+@api.route('/bird_detection')
+class BirdDetection(Resource):
+    ''' Detect bird from audio recording or caption'''
+    @api.doc('detect_bird')
+    @api.expect(parser)
+    @api.marshal_with(output, code=201)
+    def post(self):
+        ''' Receive recording or caption to detect bird'''
+        detector = BirdDetector()
+        args = parser.parse_args()
+        res = detector.predict(args)
+        return {'result': res}
+
 
 if __name__ == '__main__':
     app.run(debug=True)
--- web_api/tests/api_test.py
+++ web_api/tests/api_test.py
@@ -1,8 +1,51 @@
+import json
+import os
+from pathlib import Path
+
+import pytest
 from app import app
+from requests_toolbelt import MultipartEncoder
 
 test_app = app.test_client()
+print(os.getcwd())
 
-def test_home():
-    result = test_app.get('/')
+
+TEST_OUTPUT = [{'species': 'Corvus corvus', 'score': 0.9},
+               {'species': 'Anthus trivialis', 'score': 0.8},
+               {'species': 'Motacilla flava', 'score': 0.7},
+               {'species': 'Linaria cannabina', 'score': 0.6},
+               {'species': 'Carpodacus erythrinus', 'score': 0.5},
+               {'species': 'Acrocephalus palustris', 'score': 0.4},
+               {'species': 'Larus fuscus', 'score': 0.3},
+               {'species': 'Porzana porzana', 'score': 0.2},
+               {'species': 'Crex crex', 'score': 0.12},
+               {'species': 'Pyrrhula pyrrhula', 'score': 0.1}]
+
+curr_path = Path(__file__).parent
+sound_path = curr_path / 'test.mp3'
+img_path = curr_path / 'test.png'
+
+testdata = [MultipartEncoder(fields={'file': ('test.png',
+                                              open(img_path, 'rb'),
+                                              'image/png')}),
+            MultipartEncoder(fields={'file': ('test.mp3',
+                                              open(sound_path, 'rb'),
+                                              'audio/mp3')}),
+            MultipartEncoder(fields={'file': ('test.png',
+                                              open(img_path, 'rb'),
+                                              'image/png'),
+                                     'result': json.dumps(TEST_OUTPUT)}),
+            MultipartEncoder(fields={'file': ('test.mp3',
+                                              open(sound_path, 'rb'),
+                                              'audio/mp3'),
+                                     'result': json.dumps(TEST_OUTPUT)})
+            ]
+
+
+@pytest.mark.parametrize("data", testdata)
+def test_home(data):
+    result = test_app.post('/bird_detection', data=data,
+                           headers={'Content-Type': data.content_type})
     assert result.status_code == 200
-    assert result.data == b'Hello world'
+    assert json.loads(result.data)['result'] == TEST_OUTPUT
+

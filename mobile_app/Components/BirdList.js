6129b08ea85c52e8d6773147730e472928a9345a
Lisa Bonheme <lisa.bonheme@epitech.eu>
2019-02-19 12:40:46 +0100

    Add navigation to photo and record pages

--- mobile_app/Components/BirdList.js
+++ mobile_app/Components/BirdList.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {FlatList, View} from 'react-native';
+import {FlatList, View, TouchableOpacity, StyleSheet, Image} from 'react-native';
 import BirdItem from './BirdItem'
 import birds from '../Helpers/birdData'
 
@@ -8,18 +8,60 @@ class BirdList extends React.Component {
         this.props.navigation.navigate("Detail", {bird_name: bird_name})
     };
 
+    _displayIcon(sourceImage) {
+        return (
+          <Image
+            style={styles.icon}
+            source={sourceImage}
+          />
+        )
+    }
+
+    _addPhoto() {
+        this.props.navigation.navigate("PhotoUpload")
+    }
+
+    _addRecord() {
+        this.props.navigation.navigate("RecordUpload")
+    }
+
     render() {
         return (
-            <View>
+            <View style={styles.container}>
                 <FlatList
                     data={birds}
                     keyExtractor={(item) => item.id.toString()}
                     renderItem={({item}) => <BirdItem bird={item}
                                                       displayBirdDetail={this._displayBirdDetail}/>}
                 />
+                <View style={styles.icons_container}>
+                    <TouchableOpacity onPress={() => this._addPhoto()}>
+                        {this._displayIcon(require('../Images/camera.png'))}
+                    </TouchableOpacity>
+                    <TouchableOpacity onPress={() => this._addRecord()}>
+                        {this._displayIcon(require('../Images/record.png'))}
+                    </TouchableOpacity>
+                </View>
             </View>
         )
     }
 }
+
+const styles = StyleSheet.create({
+    icon: {
+        width: 60,
+        height: 60,
+    },
+    icons_container: {
+        flexDirection: 'row',
+        paddingLeft: 15,
+        paddingRight: 15,
+        paddingBottom: 15,
+        justifyContent: 'space-between',
+    },
+    container: {
+        flex: 1
+    }
+});
 
 export default BirdList

deb916626da05d3145814a01eac7a2d3acd59e93
Lisa Bonheme <lisa.bonheme@epitech.eu>
2019-02-26 20:42:57 +0100

    Detect bird and load their details
    Now we can upload an image to TMB api which will return
    the more likely birds.
    The bird list is then extracted and their detailed gathered to wikipedia

--- mobile_app/Components/BirdResults.js
+++ mobile_app/Components/BirdResults.js
@@ -1,42 +1,124 @@
 import React from 'react'
-import {FlatList, View, Button} from 'react-native';
+import {
+    FlatList,
+    View,
+    ActivityIndicator,
+    StyleSheet,
+    TouchableOpacity
+} from 'react-native';
 import BirdItem from './BirdItem'
-import { loadBirdsFromList } from "../API/WikiAPI";
+import {loadBirdsFromList} from "../API/WikiAPI";
+import {predictBirds} from "../API/TmbAPI";
+import {Icon} from "native-base";
 
 class BirdResults extends React.Component {
     constructor(props) {
         super(props);
-        this.state = { birds : [] }
+        this.state = {birds: undefined, results: undefined, isLoading: false}
     }
 
-    _loadBirdInfo() {
-        let bird_list = ["Bubo_bubo", "Falco_tinnunculus"];
-        loadBirdsFromList(bird_list)
-            .then((birds) => {
-                this.setState({ birds, })
-            })
-            .catch((error) => {
-                console.log(error);
-            });
+    async componentDidMount() {
+        await this.setState({isLoading: true});
+        const media = this.props.navigation.state.params.mediaURI;
+        const mediaType = this.props.navigation.state.params.mediaType;
+        const oldResults = this.props.navigation.state.params.results;
+        console.log(oldResults, media, mediaType);
+        try {
+            const {hasError, results, bird_list} = await predictBirds(media, mediaType, oldResults);
+            if (hasError) {
+                console.log(results);
+            }
+            else {
+                loadBirdsFromList(bird_list).then((birds) => {
+                    this.setState({birds, results, isLoading: false})
+                }).catch((error) => {
+                    console.log(error);
+                });
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    _displayBirdDetail = (bird_name) => {
+        this.props.navigation.navigate("Detail", {bird_name: bird_name})
+    };
+
+    _displayLoading() {
+        return (
+            <View style={styles.loading_container}>
+                <ActivityIndicator size='large'/>
+            </View>
+        )
+    }
+
+    _addPhoto() {
+        this.props.navigation.navigate("PhotoCapture", {results: this.state.results})
+    }
+
+    _addRecord() {
+        this.props.navigation.navigate("Record", {results: this.state.results})
     }
 
     render() {
+        if (this.state.isLoading) {
+            return (this._displayLoading())
+        }
         return (
-            <View>
-                <Button
-                    onPress={() => this._loadBirdInfo()}
-                    title="Load birds info"
-                    color="#841584"
-                    accessibilityLabel="Load birds info"
-                />
+            <View style={styles.container}>
                 <FlatList
                     data={this.state.birds}
                     keyExtractor={(item) => item.id.toString()}
-                    renderItem={({item}) => <BirdItem bird={item}/>}
+                    renderItem={({item}) => <BirdItem bird={item}
+                                                      displayBirdDetail={this._displayBirdDetail}/>}
                 />
+                <View style={styles.icon_container}>
+                    <TouchableOpacity onPress={() => this._addPhoto()}>
+                        <Icon android="md-camera" ios="ios-camera"
+                              style={styles.icon}/>
+                    </TouchableOpacity>
+                    <TouchableOpacity onPress={() => this._addRecord()}>
+                        <Icon android="md-mic" ios="ios-mic"
+                              style={styles.icon}/>
+                    </TouchableOpacity>
+                </View>
             </View>
         )
     }
 }
 
-export default BirdResults
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "#e2e5ec"
+    },
+    icon_container: {
+        position: 'absolute',
+        bottom: 0,
+        left: 0,
+        right: 0,
+        paddingLeft: 15,
+        paddingRight: 15,
+        paddingBottom: 15,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        backgroundColor: 'rgba(0, 0, 0, 0)'
+    },
+    icon: {
+        fontSize: 50,
+        color: "#2a2428",
+    },
+    loading_container: {
+        position: 'absolute',
+        left: 0,
+        right: 0,
+        top: 0,
+        bottom: 0,
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: "#e2e5ec"
+    }
+});
+
+export default BirdResults;

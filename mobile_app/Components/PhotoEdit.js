7bee9788a2e64c7c1371c222a15f01826c4f7820
Lisa Bonheme <lisa.bonheme@epitech.eu>
2019-02-26 13:40:22 +0100

    Edit Photo edtiion page
    Use the same design as in Record edition
    once the photo is validated, send it as props to birdResults

--- mobile_app/Components/PhotoEdit.js
+++ mobile_app/Components/PhotoEdit.js
@@ -1,26 +1,35 @@
 import React from 'react'
-import {View, Image, StyleSheet, TouchableOpacity} from 'react-native'
+import {View, Image, StyleSheet, TouchableHighlight} from 'react-native'
 import {Icon} from 'native-base'
 
 class PhotoEdit extends React.Component {
+    _sendRecord() {
+        const photo = this.props.navigation.state.params.photo;
+        this.props.navigation.navigate('Results',
+            { mediaURI: photo.uri.toString(),
+              mediaType: 'photo'});
+    }
+
     render() {
         const photo = this.props.navigation.state.params.photo;
         return (
+            <View style={styles.container}>
             <View style={styles.image_container}>
                 <Image style={styles.container}
                        source={{uri: photo.uri.toString()}}/>
-                <View style={styles.icon_container}>
-                    <TouchableOpacity onPress={() => this.props.navigation.goBack()}>
-                        <Icon android="md-trash"
-                              ios="ios-trash"
-                              style={{fontSize: 50, color: '#522'}}/>
-                    </TouchableOpacity>
-                    <TouchableOpacity>
-                        <Icon android="md-checkmark-circle-outline"
-                              ios="ios-checkmark-circle-outline"
-                              style={{fontSize: 50, color: '#241'}}/>
-                    </TouchableOpacity>
+            </View>
+            <View style={styles.button_container}>
+                <View style={styles.button_wrapper}>
+                    <TouchableHighlight style={styles.button} onPress={() => this.props.navigation.goBack()}>
+                        <Icon name={'md-trash'}
+                              style={{color: "#522", fontSize: 40}}/>
+                    </TouchableHighlight>
+                    <TouchableHighlight style={styles.button} onPress={() => {this._sendRecord()}}>
+                        <Icon name={"md-checkmark"}
+                              style={{color: "#241", fontSize: 40}}/>
+                    </TouchableHighlight>
                 </View>
+            </View>
             </View>
         );
     }
@@ -30,21 +39,26 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
     },
-    icon_container: {
-        position: 'absolute',
-        bottom: 0,
-        left: 0,
-        right: 0,
-        paddingLeft: 15,
-        paddingRight: 15,
-        paddingBottom: 15,
-        paddingTop: 15,
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        backgroundColor: 'rgba(185, 197, 213, 0.4)'
+    button_container: {
+        flex: 1,
+        backgroundColor:"#587175",
+    },
+    button: {
+        height: 60,
+        width: 60,
+        borderRadius: 40,
+        backgroundColor: "#fff",
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    button_wrapper: {
+        flexDirection: "row",
+        justifyContent: "space-around",
+        alignItems: "center",
+        paddingTop:15
     },
     image_container: {
-        flex: 1,
+        flex: 6,
         alignItems: 'stretch'
     }
 });

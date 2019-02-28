c36cf681304e5cf95154f30cf80665207011beb1
Lisa Bonheme <lisa.bonheme@epitech.eu>
2019-02-28 15:17:56 +0100

    Generate ids for bird results
    This fix id conflicts when storing the same observed birds several times
    As the id were previously wiki pages id, we ended up storing the same
    id for each page gathered several times.

--- mobile_app/API/WikiAPI.js
+++ mobile_app/API/WikiAPI.js
@@ -1,11 +1,23 @@
-// API/WikiAPI.js
+function randGenerator() {
+    return (((1+Math.random())*0x10000)|0).toString(16).substring(1);
+}
+
+function idGenerator() {
+    genId = "";
+    for (let i=0; i < 20; i++) {
+        genId += randGenerator();
+    }
+    console.log(genId);
+    return genId;
+}
+
 
 export async function getBirdFromLatinName(bird_name) {
     const url = "https://fr.wikipedia.org/api/rest_v1/page/summary/" + bird_name;
     const payload = await fetch(url);
     const jsonResponse = await payload.json();
     return {
-            id: jsonResponse.pageid,
+            id: idGenerator(),
             common_name: jsonResponse.displaytitle,
             scientific_name: bird_name,
             image_path: jsonResponse.thumbnail.source,

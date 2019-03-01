9f7bee810a797e3837e5321e816af87971cdbf7f
Lisa Bonheme <lisa.bonheme@epitech.eu>
2019-03-01 17:11:24 +0100

    Add animated splash screen and logo

--- mobile_app/App.js
+++ mobile_app/App.js
@@ -1,18 +1,63 @@
 import React from "react";
-import { Provider }  from "react-redux";
+import {Provider} from "react-redux";
+import {SplashScreen, AppLoading, Asset} from 'expo';
+import {Image, View} from 'react-native';
 import Navigation from "./Navigation/Navigation";
 import {store, persistor} from "./Store/configureStore";
-import { PersistGate } from "redux-persist/lib/integration/react";
+import {PersistGate} from "redux-persist/lib/integration/react";
 import LoadingView from "./Components/LoadingView";
 
+
 export default class App extends React.Component {
-  render() {
-      return (
-          <Provider store={store}>
-              <PersistGate loading={<LoadingView/>} persistor={persistor}>
-                  <Navigation/>
-              </PersistGate>
-          </Provider>
-      );
-  }
+
+    state = {
+        isSplashReady: false,
+        isAppReady: false,
+    };
+
+    render() {
+        if (!this.state.isSplashReady) {
+            return (
+                <AppLoading
+                    startAsync={this._cacheSplashResourcesAsync}
+                    onFinish={() => this.setState({isSplashReady: true})}
+                    onError={console.warn}
+                    autoHideSplash={false}
+                />
+            );
+        }
+
+        if (!this.state.isAppReady) {
+            return (
+                    <Image
+                        source={require('./assets/splash.gif')}
+                        onLoad={this._cacheResourcesAsync}
+                        style={{flex:1, width: null, height: null}}
+                        fadeDuration={0}
+                        resizeMode="contain"
+                    />
+            );
+        }
+
+        return (
+            <Provider store={store}>
+                <PersistGate loading={<LoadingView/>} persistor={persistor}>
+                    <Navigation/>
+                </PersistGate>
+            </Provider>
+        );
+    }
+
+    _cacheSplashResourcesAsync = async () => {
+        const gif = require('./assets/splash.gif');
+        return Asset.fromModule(gif).downloadAsync()
+    };
+
+    _cacheResourcesAsync = async () => {
+        SplashScreen.hide();
+        setTimeout(() => {
+            this.setState({isAppReady: true});
+        }, 4000);
+    }
+
 }
